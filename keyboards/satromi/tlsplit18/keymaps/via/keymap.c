a974a9141721cc1399522043eb0bd173f901887b
JetJaguar-PSX <n23m106@matsu.shimane-u.ac.jp>
2024-06-29 15:44:55 +0900

    test data uploaded

---
+++ keyboards/satromi/tlsplit18/keymaps/via/keymap.c
@@ -0,0 +1,91 @@
+/* Copyright 2020 satromi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layer_number {
+  QWERTY = 0,
+  FUNC,
+  L2,
+  L3,
+};
+
+#define MO_FUNC  MO(FUNC)
+
+
+    /* Qwerty
+        ,-----------------------------------------.              ,-----------------------------------------.
+        |   [  |   1  |   2  |   3  |   4  |   5  |              |   6  |   7  |   8  |   9  |   0  |   -  |
+ ,------+------+------+------+------+------+------+------.,------+------+------+------+------+------+------+------.
+ |  ESC |   ]  |   Q  |   W  |   E  |   R  |   T  |  DEL ||  INS |   Y  |   U  |   I  |   O  |   P  |   @  | YEN| |
+ |------+------+------+------+------+------+------+------||------+------+------+------+------+------+------+------|
+ |  HNK |  ALT |   A  |   S  |   D  |   F  |   G  |  TAB ||  ENT |   H  |   J  |   K  |   L  |   ;  |   :  |   ~  |
+ `------+------+------+------+------+------+------+------'`------+------+------+------+------+------+------+------'
+        | CTRL |   Z  |   X  |   C  |   V  |   B  |              |   N  |   M  |   ,  |   .  |   /  |   \  |
+        `-----------------------------------------'              `-----------------------------------------'
+               ,------.      ,---------------------------.,---------------------------.      ,------.
+               | PGUP |      |  SPC |  SFT |   MO |  CPS ||   JP | TEAMS|  SFT |  BSP |      |   UP |
+        +------+------+------+---------------------------'`---------------------------+------+------+------+
+        | HOME | PGDN |  END |                                                        |  LFT |  DWN |  RGT |
+        `--------------------'                                                        `--------------------'
+   */
+    /* Function
+        ,-----------------------------------------.              ,-----------------------------------------.
+        |      |   F1 |   F2 |   F3 |   F4 |   F5 |              |   F6 |   F7 |   F8 |   F9 |  F10 |      |
+ ,------+------+------+------+------+------+------+------.,------+------+------+------+------+------+------+------.
+ |      |      |      |      |      |      |      |      ||      |      |      |      |      |      |      |      |
+ |------+------+------+------+------+------+------+------||------+------+------+------+------+------+------+------|
+ | L_GUI|      |      |      |      |      |      |      ||      |      |      |      |      |      |      |      |
+ `------+------+------+------+------+------+------+------'`------+------+------+------+------+------+------+------'
+        |      |      |      |      |      |      |              |      |      |      |      |      |      |
+        `-----------------------------------------'              `-----------------------------------------'
+               ,------.      ,---------------------------.,---------------------------.      ,------.
+               |      |      |      |      |      |      ||      |      |      |      |      |      |
+        +------+------+------+---------------------------'`---------------------------+------+------+------+
+        |      |      |      |                                                        |      |      |      |
+        `--------------------'                                                        `--------------------'
+   */
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+   [QWERTY] = LAYOUT(
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO,                   KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,
+   KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_NO,                   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,  KC_NO, KC_NO,   KC_NO
+       ),
+
+   [FUNC] = LAYOUT(
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO,                   KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,
+   KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_NO,                   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,  KC_NO, KC_NO,   KC_NO
+       ),
+	
+   [L2] = LAYOUT(
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO,                   KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,
+   KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_NO,                   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,  KC_NO, KC_NO,   KC_NO
+       ),
+
+
+   [L3] = LAYOUT(
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO,                   KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,
+   KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,
+            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_NO,                   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO,  KC_NO, KC_NO,   KC_NO
+       ),
+};
+
+
