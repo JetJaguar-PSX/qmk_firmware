824df86bb5c2a9804edf505ad7dcd5918a20168a
Shiitake <64678157+JetJaguar-PSX@users.noreply.github.com>
2025-04-04 08:23:24 +0900

    Add files via upload

---
+++ keyboards/shiitake/mds_test1/keymaps/via/keymap.c
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_LGUI,
+        MS_BTN1,
+        MS_BTN2,
+        MS_BTN3
+    ),
+    [1] = LAYOUT(
+        KC_LGUI,
+        MS_BTN1,
+        MS_BTN2,
+        MS_BTN3
+    ),
+    [2] = LAYOUT(
+        KC_LGUI,
+        MS_BTN1,
+        MS_BTN2,
+        MS_BTN3
+    ),
+    [3] = LAYOUT(
+        KC_LGUI,
+        MS_BTN1,
+        MS_BTN2,
+        MS_BTN3
+    )
+};
